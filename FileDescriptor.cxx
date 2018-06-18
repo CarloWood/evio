e143ced8cea2bbfcd33ce1d598adab674e9b2663
Carlo Wood <carlo@alinoe.com>
2018-06-18 11:51:00 +0200

    WIP

--- FileDescriptor.cxx
+++ FileDescriptor.cxx
@@ -5,45 +5,95 @@
 
 namespace evio {
 
-void FileDescriptor::evio_cb(EV_P_ ev_io* w, int revents)
+void InputDevice::evio_cb(EV_P_ ev_io* w, int DEBUG_ONLY(revents))
 {
-  Dout(dc::notice, "Calling FileDescriptor::evio_cb(w, " << revents << ")");
-  if ((revents & READ))
+  Dout(dc::notice, "Calling InputDevice::evio_cb(w, " << revents << ")");
+  ASSERT(revents == EV_READ);
+  char buf[256];
+  ssize_t len;
+  do
   {
-    char buf[256];
-    ssize_t len;
-    do
-    {
-      len = read(w->fd, buf, 256);
-      Dout(dc::notice, "Read: \"" << libcwd::buf2str(buf, len) << "\".");
-    }
-    while (len == 256);
-    if (len == 5 && strncmp(buf, "quit\n", 5) == 0)
-      ev_break(EV_A_ EVBREAK_ALL);
+    len = read(w->fd, buf, 256);
+    Dout(dc::notice, "Read: \"" << libcwd::buf2str(buf, len) << "\".");
   }
-  if ((revents & WRITE))
+  while (len == 256);
+  if (strncmp(buf + len - 17, "#5</body></html>\n", 17) == 0)
   {
-    write(w->fd, "Hello world!\n", 13);
+    stop_input_device();
+    ev_break(EV_A_ EVBREAK_ALL);
   }
 }
 
-void FileDescriptor::init(int fd, events_type events)
+void OutputDevice::evio_cb(EV_P_ ev_io* w, int DEBUG_ONLY(revents))
 {
-  // Don't call init() while the FileDescriptor is already active.
+  Dout(dc::notice, "Calling OutputDevice::evio_cb(w, " << revents << ")");
+  ASSERT(revents == EV_WRITE);
+  static int request = 0;
+  if (request < 6)
+  {
+    std::stringstream ss;
+    ss << "GET / HTTP/1.1\r\nHost: localhost:9001\r\nAccept: */*\r\nX-Request: " << request++ << "\r\nX-Sleep: 100\r\n\r\n";
+    write(w->fd, ss.str().data(), ss.str().length());
+    Dout(dc::notice, "Wrote \"" << libcwd::buf2str(ss.str().data(), ss.str().length()) << "\".");
+  }
+  else
+  {
+    stop_output_device();
+  }
+}
+
+void InputDevice::init_input_device(int fd)
+{
+  // Don't call init() while the InputDevice is already active.
   ASSERT(!is_active());
-  // events must be READ, WRITE or READ|WRITE.
-  ASSERT(events != NONE && events == (events & (READ|WRITE)));
-  ev_io_init(&m_io, FileDescriptor::s_evio_cb, fd, events);
-  m_io.data = this;
+  ev_io_init(&m_input_watcher, InputDevice::s_evio_cb, fd, EV_READ);
+  m_input_watcher.data = this;
 }
 
-void FileDescriptor::start(EventLoopThread& evio_loop)
+void OutputDevice::init_output_device(int fd)
 {
-  // Call FileDescriptor::init before calling FileDescriptor::start.
-  ASSERT(m_io.events != EV_UNDEF);
+  // Don't call init() while the OutputDevice is already active.
+  ASSERT(!is_active());
+  ev_io_init(&m_output_watcher, OutputDevice::s_evio_cb, fd, EV_WRITE);
+  m_output_watcher.data = this;
+}
+
+void InputDevice::start_input_device(EventLoopThread& evio_loop)
+{
+  // Call InputDevice::init before calling InputDevice::start.
+  ASSERT(m_input_watcher.events != EV_UNDEF);
   // Don't call start twice on a row.
   ASSERT(!is_active());
-  evio_loop.start(m_io);
+  evio_loop.start(m_input_watcher);
+  intrusive_ptr_add_ref(this);
+}
+
+void OutputDevice::start_output_device(EventLoopThread& evio_loop)
+{
+  // Call OutputDevice::init before calling OutputDevice::start.
+  ASSERT(m_output_watcher.events != EV_UNDEF);
+  // Don't call start twice on a row.
+  ASSERT(!is_active());
+  evio_loop.start(m_output_watcher);
+  intrusive_ptr_add_ref(this);
+}
+
+void InputDevice::stop_input_device()
+{
+  if (is_active())
+  {
+    ev_io_stop(EV_A_ &m_input_watcher);
+    intrusive_ptr_release(this);
+  }
+}
+
+void OutputDevice::stop_output_device()
+{
+  if (is_active())
+  {
+    ev_io_stop(EV_A_ &m_output_watcher);
+    intrusive_ptr_release(this);
+  }
 }
 
 } // namespace evio
--- FileDescriptor.h
+++ FileDescriptor.h
@@ -8,30 +8,98 @@ class EventLoopThread;
 
 namespace evio {
 
-class FileDescriptor : public AIRefCount
+// Virtual base class for IO Devices.
+//
+// This class takes care of the life-time of an Input-, Output- or IO-Device.
+class IOBase : public AIRefCount
 {
- private:
-  friend EventLoopThread; // Needs access to m_io.
-  ev_io m_io;
+ protected:
+  virtual void init_input_device(int) { }
+  virtual void init_output_device(int) { }
+  virtual void start_input_device(EventLoopThread&) { }
+  virtual void start_output_device(EventLoopThread&) { }
+  virtual void stop_input_device() { }
+  virtual void stop_output_device() { }
 
- private:
-   void evio_cb(EV_P_ ev_io* w, int revents);
-   static void s_evio_cb(EV_P_ ev_io* w, int revents) { static_cast<FileDescriptor*>(w->data)->evio_cb(w, revents); }
+#ifdef CWDEBUG
+  ~IOBase() { Dout(dc::notice, "Destructing IOBase [" << (void*)this << "]"); }
+#endif
 
  public:
-  FileDescriptor()
+  // (Re)Initialize the FileDescriptor.
+  void init(int fd)
   {
-    // Give m_io known values; cause is_active() to return false.
-    ev_io_init(&m_io, FileDescriptor::s_evio_cb, -1, EV_UNDEF);
+    init_input_device(fd);
+    init_output_device(fd);
   }
 
-  // (Re)Initialize the FileDescriptor.
-  void init(int fd, events_type events);
+  void start(EventLoopThread& evio_loop)
+  {
+    start_input_device(evio_loop);
+    start_output_device(evio_loop);
+  }
 
-  // Add filedescriptor to libev for monitoring.
-  void start(EventLoopThread& evio_loop);
+  void stop()
+  {
+    stop_input_device();
+    stop_output_device();
+  }
+};
 
-  bool is_active() const { return ev_is_active(&m_io); }
+class InputDevice : public virtual IOBase
+{
+ private:
+  friend EventLoopThread; // Needs access to m_input_watcher.
+  ev_io m_input_watcher;
+
+ private:
+  void evio_cb(EV_P_ ev_io* w, int revents);
+  static void s_evio_cb(EV_P_ ev_io* w, int revents) { static_cast<InputDevice*>(w->data)->evio_cb(w, revents); }
+
+  void init_input_device(int fd) override;
+  void start_input_device(EventLoopThread& evio_loop) override;
+  void stop_input_device() override;
+
+ public:
+  InputDevice()
+  {
+    // Give m_input_watcher known values; cause is_active() to return false.
+    ev_io_init(&m_input_watcher, InputDevice::s_evio_cb, -1, EV_UNDEF);
+  }
+
+  bool is_active() const { return ev_is_active(&m_input_watcher); }
+
+ protected:
+  int fd() const { return m_input_watcher.fd; }
+  virtual void read_from_fd();
+};
+
+class OutputDevice : public virtual IOBase
+{
+ private:
+  friend EventLoopThread; // Needs access to m_output_watcher.
+  ev_io m_output_watcher;
+
+ private:
+  void evio_cb(EV_P_ ev_io* w, int revents);
+  static void s_evio_cb(EV_P_ ev_io* w, int revents) { static_cast<OutputDevice*>(w->data)->evio_cb(w, revents); }
+
+  void init_output_device(int fd) override;
+  void start_output_device(EventLoopThread& evio_loop) override;
+  void stop_output_device() override;
+
+ public:
+  OutputDevice()
+  {
+    // Give m_output_watcher known values; cause is_active() to return false.
+    ev_io_init(&m_output_watcher, OutputDevice::s_evio_cb, -1, EV_UNDEF);
+  }
+
+  bool is_active() const { return ev_is_active(&m_output_watcher); }
+
+ protected:
+  int fd() const { return m_output_watcher.fd; }
+  virtual void write_to_fd();
 };
 
 } // namespace evio
