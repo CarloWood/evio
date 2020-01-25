ca174fa6d6fa1e4b543216be7071213112c00399
Carlo Wood <carlo@alinoe.com>
2020-01-25 17:20:16 +0100

    W.I.P.

--- protocol/configure_matrixssl.sh
+++ protocol/configure_matrixssl.sh
@@ -11,10 +11,18 @@ fi
 MATRIXSSL_SRCDIR="$(dirname $0)/matrixssl"
 export CFLAGS="-Wno-unused-function -Wno-\\#warnings"
 
+CCACHE_STR=""
+if [ -n "$CCACHE_DIR" ]; then
+  CCACHE="`which ccache`"
+  if [ -n "$CCACHE" ]; then
+    eval CCACHE_STR=' CC="$CCACHE $CC" CXX="$CCACHE $CXX"'
+  fi
+fi
+
 CONFIGURE_OPTIONS="$1"
 shift
-echo "Running '\"$MATRIXSSL_SRCDIR\"/configure $CONFIGURE_OPTIONS --enable-maintainer-mode' ..."
-"$MATRIXSSL_SRCDIR"/configure $CONFIGURE_OPTIONS --enable-maintainer-mode || exit 1
+echo "Running '\"$MATRIXSSL_SRCDIR\"/configure$CCACHE_STR $CONFIGURE_OPTIONS --enable-maintainer-mode' ..."
+"$MATRIXSSL_SRCDIR"/configure$CCACHE_STR $CONFIGURE_OPTIONS --enable-maintainer-mode || exit 1
 echo "Running $@ list-configure-files ..."
 "$@" list-configure-files 2>/dev/null | /bin/grep -v ' ' > "$MATRIXSSL_SRCDIR/../matrixssl-configure-files.new" && \
   mv "$MATRIXSSL_SRCDIR/../matrixssl-configure-files.new" "$MATRIXSSL_SRCDIR/../matrixssl-configure-files"
